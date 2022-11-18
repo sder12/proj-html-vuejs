71473d72621e7fbe2121860e7009f4a0f78dab9f
sder12 <sara.cetto@gmail.com>
2022-11-18 10:00:54 +0100

    Basic layout each main section

--- src/components/AppBranches.vue
+++ src/components/AppBranches.vue
@@ -5,11 +5,64 @@ export default {
 </script>
 
 <template>
-    <h2>AppBranches</h2>
+    <div class="branches">
+        <!-- New York -->
+        <div class="branch ny">
+            <div class="branch__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus placeat quia ullam dicta
+                voluptate
+                tenetur laboriosam maxime quis fugit, atque inventore, earum ipsum corporis sequi magnam culpa.
+                Doloremque,
+                quod.
+            </div>
+            <div class="branch__txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus placeat quia ullam dicta
+                voluptate
+                tenetur laboriosam maxime quis fugit, atque inventore, earum ipsum corporis sequi magnam culpa.
+                Doloremque,
+                quod.
+            </div>
+
+        </div>
+        <!-- London -->
+        <div class="branch london">
+            <div class="branch__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus placeat quia ullam dicta
+                voluptate
+                tenetur laboriosam maxime quis fugit, atque inventore, earum ipsum corporis sequi magnam culpa.
+                Doloremque,
+                quod.
+            </div>
+            <div class="branch__txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus placeat quia ullam dicta
+                voluptate
+                tenetur laboriosam maxime quis fugit, atque inventore, earum ipsum corporis sequi magnam culpa.
+                Doloremque,
+                quod.
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-h2 {
-    background-color: rgb(157, 244, 255);
+@use "../styles/partials/variables" as*;
+@use "../styles/partials/mixins" as*;
+
+.branches {
+    @include flex(row, center, center);
+
+    //DEBUG
+    >* {
+        background-color: rgb(157, 244, 255);
+    }
+
+    .branch {
+        //DEBUG
+        border: 1px solid blue;
+
+        >* {
+            background-color: rgba(6, 8, 124, 0.171);
+        }
+    }
 }
 </style>
--- src/components/AppSharing.vue
+++ src/components/AppSharing.vue
@@ -5,11 +5,75 @@ export default {
 </script>
 
 <template>
-    <h2>AppSharing</h2>
+    <div class="sharing">
+        <div class="row sharing__section">
+            <div class="sharing__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+            <div class="sharing__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+            <div class="sharing__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+        </div>
+        <div class="row sharing__section">
+            <div class="sharing__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+            <div class="sharing__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+            <div class="sharing__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+            <div class="sharing__img">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad eos temporibus reiciendis architecto
+                nam neque animi, saepe quasi itaque, pariatur ex velit atque, maiores minus laborum consectetur officiis
+                et!
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <style lang="scss" scoped>
-h2 {
-    background-color: rgb(74, 0, 247);
+@use "../styles/partials/variables" as*;
+@use "../styles/partials/mixins" as*;
+
+.sharing {
+    @include flex(column, center, center);
+    background-color: lightblue;
+
+    &__section {
+        @include flex(row, center, center);
+        gap: 2em;
+
+        >* {
+            border: 1px solid black
+        }
+
+        ;
+
+        .sharing__img {
+            width: 25%;
+        }
+
+        .sharing__text {
+            width: 50%;
+        }
+    }
 }
 </style>

57a54af4d034bc5cdc3e9fa6e6001adeaa1a7a76
sder12 <sara.cetto@gmail.com>
2022-11-20 11:43:02 +0100

    Gallery events refactoring, card + hover

--- src/components/AppGallery.vue
+++ src/components/AppGallery.vue
@@ -1,24 +1,42 @@
 <script>
+import AppGalleryCard from './AppGalleryCard.vue';
 export default {
-    name: "AppGallery"
+    name: "AppGallery",
+    components: { AppGalleryCard },
+    data() {
+        return {
+            galleryEvents: [
+                {
+                    imgVisible: "../assets/img/gallery/corporate-bg.jpg",
+                    imgAlt: "corporate",
+                    imgHover: "../assets/img/gallery/corporate-hover-bg.jpg",
+                    hoverTitle: "Corporate events",
+                    hoverSubTitle: "our product",
+                    hoverBtn: "Explore more"
+                },
+                {
+                    imgVisible: "../assets/img/gallery/wedding-bg.jpg",
+                    imgAlt: "wedding",
+                    imgHover: "../assets/img/gallery/wedding-hover-bg.jpg",
+                    hoverTitle: "Wedding & Parties",
+                    hoverSubTitle: "private dinning hall",
+                    hoverBtn: "Explore more"
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <section class="ms_gallery container-fluid g-0">
         <div class="row g-3">
-            <div class="col-lg-6">
-                <img src="../assets/img/corporate-bg.jpg" alt="">
-            </div>
-            <div class="col-lg-6">
-                <img src="../assets/img/wedding-bg.jpg" alt="">
-            </div>
+            <AppGalleryCard v-for="(events, index) in galleryEvents" :key="index" :galleryImg="events" />
         </div>
     </section>
 
 </template>
 
 <style lang="scss" scoped>
-@use "../styles/partials/variables" as*;
-@use "../styles/partials/mixins" as*;
+
 </style>
---
+++ src/components/AppGalleryCard.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+    name: "AppGalleryCard",
+    props: {
+        galleryImg: Object
+    },
+    methods: {
+        getImage(pathImg) {
+            return new URL(pathImg, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="ms_gallery-img col-lg-6 ">
+
+        <!-- STATIC-->
+        <div class="position-relative">
+            <!-- Img Visible -->
+            <img :src="getImage(galleryImg.imgVisible)" :alt="imgAlt">
+
+            <!-- HOVER -->
+            <div class="ms_gallery-hover">
+                <!-- Img Visible with hover -->
+                <img :src="getImage(galleryImg.imgHover)" :alt="imgAlt">
+                <!-- Text Visible with hover -->
+                <div class="ms_gallery-hover-txt text-center ">
+                    <span class="ms_little-caption">
+                        {{ galleryImg.hoverSubTitle }}
+                    </span>
+                    <h3>
+                        {{ galleryImg.hoverTitle }}
+                    </h3>
+                    <button class="btn ms_btn-dark">
+                        {{ galleryImg.hoverBtn }}
+                    </button>
+                </div>
+            </div>
+            <!-- / HOVER -->
+        </div>
+        <!-- /STATIC-->
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+.ms_gallery-img:hover .ms_gallery-hover {
+    opacity: 1;
+}
+
+.ms_gallery-hover {
+    opacity: 0;
+    background-color: black;
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    img {
+        opacity: .5;
+    }
+
+    &-txt {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        h3,
+        span {
+            color: white;
+        }
+    }
+
+
+}
+</style>

807b6f043eb8d4f60db715d9421185698b6743c8
sder12 <sara.cetto@gmail.com>
2022-11-20 11:43:55 +0100

    Values section refactoring, cards + data

--- src/components/AppValues.vue
+++ src/components/AppValues.vue
@@ -1,61 +1,75 @@
 <script>
+import AppValuesCard from './AppValuesCard.vue';
 export default {
-    name: "AppValues"
+    name: "AppValues",
+    components: { AppValuesCard },
+    data() {
+        return {
+            quote: {
+                subTitle: "dont't just take our word for it",
+                sentence: "Finally found an alternative to the mass products. Something that incorporates real organic ingredients, nutrients dense wellness while promoting sustainabilit andd activity.",
+                cit: "Rachel Copper, Founder"
+            },
+            valueOne: {
+                valueNumber: "01",
+                title: "Tell us what you have in mind",
+                subTitle: "manual ingredients selection",
+                paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                srcImg: "../assets/img/values/bakery-process-1.jpg",
+                altImg: "Bakery process",
+                align: "end",
+            },
+            valueSecond: {
+                valueNumber: "02",
+                title: "We Start Baking",
+                subTitle: "sweet & delicious",
+                paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                srcImg: "../assets/img/values/bakery-process-2.jpg",
+                altImg: "Bakery process",
+                align: "start",
+            },
+            valueThird: {
+                valueNumber: "03",
+                title: "Delivered to your home",
+                subTitle: "baked to order & gift wrapper",
+                paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+                srcImg: "../assets/img/values/bakery-process-3.jpg",
+                altImg: "Bakery process",
+                align: "start",
+            }
+        }
+    }
 }
 </script>
 
 <template>
     <section class="ms_values container-fluid g-0">
-        <div class="row  align-items-stretch gy-4 gx-0">
+        <div class="row align-items-stretch gy-4 gx-0">
 
-
-            <div class="ms_values-one col-12">
-                <div class="d-flex flex-column justify-content-between">
-                    <div class="d-flex justify-content-between">
-                        <h3>Tell us what you have in mind</h3>
-                        <h2>01</h2>
-                    </div>
-                    <div class="align-self-end text-end">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                    </div>
-                </div>
-
+            <!-- Value One -->
+            <div class="overlay col-12">
+                <AppValuesCard :value="valueOne" />
             </div>
 
-            <div class="ms_values-two col-6 ">
-                <div class="d-flex flex-column justify-content-between">
-                    <div class="d-flex justify-content-between">
-                        <h3>We Start Baking</h3>
-                        <h2>02</h2>
-                    </div>
-                    <div class="align-self-start">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                    </div>
-                </div>
+            <!-- Value Second -->
+            <div class="overlay col-6 ">
+                <AppValuesCard :value="valueSecond" />
             </div>
 
+            <!-- Quote section -->
             <div class="ms_values-text col-6 ">
                 <div class="d-flex justify-content-center align-items-center ms-4">
                     <div>
-                        <span>dont't just take our word for it</span>
-                        <h3>"Finally found an alternatice to the mass products. Something that incorporates real organic
-                            ingredients, nutrients dense wellness while promoting sustainabilit andd activity." </h3>
-                        <span>Rachel Copper, Founder</span>
+                        <span class="ms_little-caption">{{ quote.subTitle }}</span>
+                        <h3>&ldquo;{{ quote.sentence }}&rdquo;</h3>
+                        <span class="ms_little-caption text-capitalize">{{ quote.cit }}</span>
                     </div>
                 </div>
             </div>
 
-
-            <div class="ms_value-three col-12">
-                <div class="d-flex flex-column justify-content-between">
-                    <div class="d-flex justify-content-between">
-                        <h3>Delivered to your home</h3>
-                        <h2>03</h2>
-                    </div>
-                    <div class="align-self-start">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-                    </div>
-                </div>
+            <!-- Value Third -->
+            <div class="overlay col-12">
+                <AppValuesCard :value="valueThird" />
             </div>
 
         </div>
@@ -64,40 +78,16 @@ export default {
 
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as*;
-@use "../styles/partials/mixins" as*;
+
+.overlay {
+    background-color: black;
+}
 
 .ms_values-text {
-
     >* {
         height: 100%;
         background-color: $bg-simple;
-        padding: 3em 4em;
+        padding: 0 4em;
     }
-}
-
-.ms_values-one,
-.ms_values-two,
-.ms_value-three {
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    color: white;
-    padding: 2em 2em;
-
-    >* {
-        height: 100%;
-    }
-}
-
-.ms_values-one {
-    background-image: url("../assets/img/values/bakery-process-1.jpg");
-}
-
-.ms_values-two {
-    background-image: url("../assets/img/values/bakery-process-2.jpg");
-}
-
-.ms_value-three {
-    background-image: url("../assets/img/values/bakery-process-3.jpg");
 }
 </style>
---
+++ src/components/AppValuesCard.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+    name: "AppValuesCard",
+    props: {
+        value: Object
+    },
+    methods: {
+        getImage(pathImg) {
+            return new URL(pathImg, import.meta.url).href
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="ms_value position-relative">
+        <!-- ImG Background -->
+        <img :src="getImage(value.srcImg)" :alt="value.altImg">
+
+        <!-- TEXT in absolute -->
+        <div class="ms_value-txt position-absolute top-0 start-0">
+            <div class="ms_value-txt-inner">
+
+                <!-- TOP Title + number -->
+                <div class="ms_value-txt-inner-title">
+                    <h2 class="ms-values-title">
+                        {{ value.title }}
+                    </h2>
+                    <h2 class="ms_values-numbers">
+                        {{ value.valueNumber }}
+                    </h2>
+                </div>
+
+                <!-- BOTTOM Subtitle + Paragraph -->
+                <div :class="value.align == 'end' ? 'align-self-end text-end' : 'align-self-start'"
+                    class="ms_value-txt-white">
+                    <span class="ms_little-caption">
+                        {{ value.subTitle }}
+                    </span>
+                    <p>
+                        {{ value.paragraph }}
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as*;
+@use "../styles/partials/mixins" as*;
+
+// IMAGE
+img {
+    filter: opacity(0.7);
+}
+
+// TEXT
+.ms_value-txt {
+    padding: 2em 3em;
+    width: 100%;
+    height: 100%;
+
+    &-inner {
+        height: 100%;
+        @include flex(column, space-between, space-between);
+
+        &-title {
+            @include flex(row, space-between, space-between)
+        }
+    }
+
+    &-white {
+        color: $lighten-txt;
+    }
+}
+</style>

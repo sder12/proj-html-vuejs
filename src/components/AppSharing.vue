70bbf000089f79b3d88c0187ac671b60e1f796cc
sder12 <sara.cetto@gmail.com>
2022-11-20 11:44:57 +0100

    Sharing section, images v-for

--- src/components/AppSharing.vue
+++ src/components/AppSharing.vue
@@ -1,21 +1,59 @@
 <script>
 export default {
-    name: "AppSharing"
+    name: "AppSharing",
+    data() {
+        return {
+            sharingImg: [
+                {
+                    imgSrc: "../assets/img/cta-sharing/social-1.jpg",
+                    imgAlt: "woman baking",
+                    order: 1,
+                },
+                {
+                    imgSrc: "../assets/img/cta-sharing/social-2.jpg",
+                    imgAlt: "croissant",
+                    order: 3,
+                },
+                {
+                    imgSrc: "../assets/img/cta-sharing/social-3.jpg",
+                    imgAlt: "bread",
+                    order: 3,
+
+                },
+                {
+                    imgSrc: "../assets/img/cta-sharing/social-4.jpg",
+                    imgAlt: "bread",
+                    order: 3,
+                },
+                {
+                    imgSrc: "../assets/img/cta-sharing/social-5.jpg",
+                    imgAlt: "crepes",
+                    order: 3,
+                },
+                {
+                    imgSrc: "../assets/img/cta-sharing/social-6.jpg",
+                    imgAlt: "coffee",
+                    order: 3,
+                },
+
+            ]
+        }
+    },
+    methods: {
+        getImage(pathImg) {
+            return new URL(pathImg, import.meta.url).href
+        }
+    }
 }
 </script>
 
 <template>
-    <section class="ms_cta container-fluid g-0">
 
-
+    <section class="container-fluid g-0">
         <div class="ms_sharing row g-3 align-items-stretch">
 
-            <div class="ms_sharing__img col-3 ">
-                <img src="../assets/img/cta-sharing/social-1.jpg" alt="">
-            </div>
-
-
-            <div class="ms_sharing__text col-6 ">
+            <!-- TeXT -->
+            <div class="ms_sharing__text col-6 order-2">
                 <div class=" text-center">
                     <span class="ms_little-caption">made for sharing</span>
                     <h4>Let's stay in touch</h4>
@@ -26,32 +64,19 @@ export default {
                 </div>
             </div>
 
-
-            <div class="ms_sharing__img col-3 ">
-                <img src="../assets/img/cta-sharing/social-2.jpg" alt="">
-            </div>
-
-            <div class="ms_sharing__img col-3 ">
-                <img src="../assets/img/cta-sharing/social-3.jpg" alt="">
-            </div>
-            <div class="ms_sharing__img col-3 ">
-                <img src="../assets/img/cta-sharing/social-4.jpg" alt="">
-            </div>
-            <div class="ms_sharing__img col-3 ">
-                <img src="../assets/img/cta-sharing/social-5.jpg" alt="">
-            </div>
-            <div class="ms_sharing__img col-3 ">
-                <img src="../assets/img/cta-sharing/social-6.jpg" alt="">
+            <!-- All images -->
+            <div class="ms_sharing__img col-3" v-for="(photo, index) in sharingImg" :key="index"
+                :class="`order-${photo.order}`">
+                <img :src="getImage(photo.imgSrc)" :alt="photo.imgAlt">
             </div>
 
         </div>
-
     </section>
+
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as*;
-@use "../styles/partials/mixins" as*;
 
 .ms_sharing {
     &__text {
